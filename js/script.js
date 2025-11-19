54419af3ad9c2f1f6d2c08fcb30af2afc05befeb
poncianodev <lucasbponciano@hotmail.com>
2025-11-18 22:05:25 -0300

    feat: foi adicionado o slider das marcas

--- js/script.js
+++ js/script.js
@@ -128,3 +128,8 @@ ulMenu.querySelectorAll("a").forEach((link) => {
     ulMenu.classList.remove("open");
   });
 });
+
+// SLIDE LOGOS
+
+let copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
